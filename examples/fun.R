4703f61390745b6fa26b02ffee8dfa6b632f0880
vincent <vincent@yeswescore.com>
2018-02-15 01:08:25 +0100

    add example with ngram

--- examples/fun.R
+++ examples/fun.R
@@ -1,1 +1,26 @@
+# install.packages("ngram")
+# install.packages("tm")
 
+library(ngram)
+
+# read txt file
+url <- "https://raw.githubusercontent.com/voltek62/RsparkleR-examples/master/examples/advs.txt"
+txt <- readLines(url)
+data.sentence <- concatenate(txt)
+
+# remove punctuations & numbers, fix spacing
+data.sentence.staging <- preprocess(data.sentence
+                                    ,case='lower'
+                                    ,remove.punct = TRUE 
+                                    ,remove.numbers = TRUE
+                                    ,fix.spacing = TRUE
+                                    )
+
+# remove stopwords
+stopwords_regex = paste(c(stopwords('en'),'holmes'), collapse = '\\b|\\b')
+stopwords_regex = paste0('\\b', stopwords_regex, '\\b')
+data.sentence.prepared = stringr::str_replace_all(data.sentence.staging, stopwords_regex, '')
+
+ng <- ngram(data.sentence.prepared, n=2)
+
+print(head(get.phrasetable(ng)))

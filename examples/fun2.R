f1f8202eb6c78d4a33f72f75bff037a280919741
Vincent Terrasi <vt3ktek@hotmail.com>
2018-05-27 22:20:00 +0200

    add test

---
+++ examples/fun2.R
@@ -0,0 +1,12 @@
+# library(devtools)
+# install_github("voltek62/caesaR")
+
+library(caesaR)
+
+crypted <- "ndmha, hage mhql dqgeeu m pqoapqd oq oapq dqxmfuhqyqzf xazs qz gfuxuemzf gz bdasdmyyq qz d. bagd bdaghqd ym nazzq rau vq hage pazzq xq oapq nmwqd bagd aghdud xq oarrdq vq hage oazequxxq pq smdpqd xq eqodqf bagd hage. msqzf h"
+
+# si le code n'est pas lisible, faites varier la distance de 3 à 24
+caesar(crypted, distance = 2, reverse=TRUE)
+
+
+
